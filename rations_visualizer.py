42d2798d8a654d3d97338a539bd29b0d8f937e14
Andrew Tran <andrew.c.tran1@gmail.com>
2020-11-25 16:47:56 -0800

    Create mapping of each ingredient to the amount that is available each day assuming rations are divided normally

--- rations_visualizer.py
+++ rations_visualizer.py
@@ -1,6 +1,8 @@
 import streamlit
 import string
 from airtable import Airtable
+from collections import OrderedDict
+from datetime import datetime, timedelta
 
 # 1) Connect with Airtable.
 AIRTABLE_API_KEY = "keygCUTG6e5DvySOR"
@@ -29,6 +31,7 @@ api_response = airtable.get_all()
 # 	},
 # }
 announcements = {}
+ingredient_to_date_to_amounts = {}
 for thing in api_response:
 	data = thing["fields"]
 
@@ -39,24 +42,28 @@ for thing in api_response:
 		start_date = data["Date"]
 
 	if "Est. Duration" in data:
-		# "Est. Duration" appears in the Airtable usually as "X days" or "X days (per coupon)"
+		# "Est. Duration" appears in the Airtable usually as "X days"/"X days (per coupon)"/"X week"
 		# Because it was always formatted this way, we can always rely on the number (of days) 
 		# being the numerical value before the first whitespace
 		duration_in_days = int(data["Est. Duration"].split(" ")[0])
+		if "week" in data["Est. Duration"]:
+			duration_in_days *= 7
 	else:
-		# Assume indefinite duration
-		duration_in_days = None
+		# Assume 10 day duration otherwise
+		duration_in_days = 10
 
 	items = {}
 	for key in data:
 		if "(g)" in key:
 			items[key] = data[key]
+			ingredient_to_date_to_amounts[key] = {}
 
 	announcements[data["Date"]] = {
 		"start_date": start_date,
 		"duration_in_days": duration_in_days,
 		"items": items,
 	}
+announcements = OrderedDict(sorted(announcements.items()))
 
 # 3) At this point, we have a Python dictionary named 'announcements' that holds all of our data in a easily iterable format.
 streamlit.write(announcements)
@@ -64,13 +71,31 @@ streamlit.write(announcements)
 # 4) [TO-DO] Transform the 'announcements' dictionary into a different dictionary with the format:
 # 
 # {
-# 	"Zucker/Sugar (g)": [50, 50, 50, ...],
-# 	"Salz/Salt (g)": [30, 30, 30, 20, ...]
+# 	"Zucker/Sugar (g)": {
+# 		"1940-12-25": 50,
+# 		"1940-12-26": 50,
+# 		...
+# 	}
 # }
 # 
 # Mapping each provision to list of length ~1200 (each index representating the nth day since
 # the first ration announcement in the Chronicle) where the value at each index is the amount
 # of mass of that provision that can be consumed that day. All lists should be the same length.
+final_date = datetime.strptime(list(announcements.keys())[-1], "%Y-%m-%d")		# Get the date of the final announcement from the Chronicles
+
+for announcement_date, announcement_info in announcements.items():
+	for ingredient, ration_amount in announcement_info["items"].items():
+		ration_effective_start_date = datetime.strptime(announcement_info["start_date"], "%Y-%m-%d")
+		ration_effective_duration = announcement_info["duration_in_days"]
+		for days_since_start in range(ration_effective_duration):
+			current_date = ration_effective_start_date + timedelta(days_since_start)
+			ingredient_to_date_to_amounts[ingredient][current_date.strftime("%Y-%m-%d")] = ration_amount / ration_effective_duration
+
+# Order each ingredient's mapping of dates to amount available on that date by dates in ascending order.
+for ingredient in ingredient_to_date_to_amounts.keys():
+	ingredient_to_date_to_amounts[ingredient] = OrderedDict(sorted(ingredient_to_date_to_amounts[ingredient].items()))
+
+streamlit.write(ingredient_to_date_to_amounts)
 
 # Render a slider on the page.
 # Example: streamlit.slider(label, min_value=None, max_value=None, value=None, step=None, format=None, key=None)
